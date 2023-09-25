24ad94a9e2ca93bcce4a7aa766b5232ac2037ef4
marwansrg27 <marwansargha27@gmail.com>
2023-09-25 14:54:21 +0200

    lab2 3.8

--- index.js
+++ index.js
@@ -17,21 +17,7 @@ const content = '<!DOCTYPE html>' +
 const url = require('url')
 const qs = require('querystring')
 
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-
-  res.writeHead(200, {'Content-Type': 'text/plain'})
-
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-  
-  res.end()
-}
+const handles = require('./handles')
 
 http
 .createServer(serverHandle)
