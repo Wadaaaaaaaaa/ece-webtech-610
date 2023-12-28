f4a62279f2c57ea0a894a5c53a1536c52a3040b1
marwansrg27 <marwansargha27@gmail.com>
2023-12-28 16:43:48 +0100

    fix: suppression du node_modules du dossier server

--- server/index.js
+++ server/index.js
@@ -17,3 +17,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Server running on port ${PORT}');
 });
+
+const commentairesRoutes = require('./routes/commentairesRoutes');
+
+app.use('/commentaires', commentairesRoutes(supabase));
